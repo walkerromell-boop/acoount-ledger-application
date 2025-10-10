01fa7ddefe2bb3954c6878b4b9eff314beb9c020
Romell Walker <walkerromell@gmail.com>
2025-10-10 15:03:05 -0400

    added the field members with getters and setters also added toString

--- src/main/java/com/pluralsight/ApplicationApp.java
+++ src/main/java/com/pluralsight/ApplicationApp.java
@@ -1,7 +1,47 @@
 package com.pluralsight;
 
-public class ApplicationApp {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class ApplicationApp {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+        try {
+            FileReader fileReader = new FileReader("transactions.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            boolean hasContent = false;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                hasContent = true;
+                System.out.println(line); // shows what was read
+            }
+
+            if (hasContent) {
+                System.out.println(" File read successfully!");
+            } else {
+                System.out.println(" File is empty.");
+            }
+            bufferedReader.close();
+        } catch (
+                FileNotFoundException e) {
+            System.out.println(" File not found: " + e.getMessage());
+        } catch (
+                IOException e) {
+            System.out.println(" Error reading file: " + e.getMessage());
+        }
     }
 }
+
--- src/main/java/com/pluralsight/Transactions.java
+++ src/main/java/com/pluralsight/Transactions.java
@@ -1,4 +1,48 @@
 package com.pluralsight;
 
 public class Transactions {
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Transactions(String date, String time, String description,String vendor, double amount){
+        this.date=date;
+        this.time=time;
+        this.description=description;
+        this.vendor=vendor;
+        this.amount=amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transactions{" +
+                "date='" + date + '\'' +
+                ", time='" + time + '\'' +
+                ", description='" + description + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
